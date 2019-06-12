53a1a501a8fe8f9e87cc98f158c385457a7bb103
salgoo4u <51224038+salgoo4u@users.noreply.github.com>
2019-06-12 16:24:47 +0900

    Modify file via upload
    이전 수정 시 반영하지 못 한 comment 추가 및 줄바꿈 관련 부분 추가로 수정했습니다.

--- priority_queue_test.cc
+++ priority_queue_test.cc
@@ -24,30 +24,32 @@ templatePriorityQueue<int> expTarget;
 
   ASSERT_EQ(expTarget.size(),array_size); // Pushed 6 elements
   ASSERT_EQ(expTarget.top(),8);  // The highest pushed value is 8
-  ASSERT_FALSE(expTarget.empty()); // it must not empty
+  ASSERT_FALSE(expTarget.empty()); // it must not be empty
 
-  //Test 3 - pop test
+  //Test 3 - Pop elements of 8, 5, 4 test
   for (int i = 0; i < pop_test_count; i++)
   {
     expTarget.pop();
   }
 	
-  ASSERT_EQ(expTarget.top(),3);
-  ASSERT_EQ(expTarget.size(),array_size-pop_test_count);
-  ASSERT_FALSE(expTarget.empty());
+  ASSERT_EQ(expTarget.top(),3); // The remained highest pushed value is 3
+  ASSERT_EQ(expTarget.size(),array_size-pop_test_count); // The remained elements is array_size-pop_test_count
+  ASSERT_FALSE(expTarget.empty()); // it must not empty
 
+  //Test 4 - Pop remained elements
   for (int i = 0; i < array_size-pop_test_count; i++)
   {
     expTarget.pop();
   }
 	
-  ASSERT_TRUE(expTarget.empty());
+  ASSERT_TRUE(expTarget.empty()); // it must be empty
 	
   /* Fail Example - If you activate this code, Test will be fail
 
   expTarget.pop();
-	
-  EXPECT_TRUE(expTarget.empty()); // non-Fatal Failure - not terminate this test case when result of empty method is false.
+  
+  // non-Fatal Failure - not terminate this test case when result of empty method is false.
+  EXPECT_TRUE(expTarget.empty()); 
   ASSERT_EQ(expTarget.size(),0);
   */
 }
